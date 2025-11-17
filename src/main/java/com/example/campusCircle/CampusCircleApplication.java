b89a0bdb9cb929da4167d38b4be78616a045b6c4
billelz <billel.zemmel@craftschoolship.com>
2025-11-17 19:37:50 +0100

    Initial commit: Spring Boot campusCircle project

---
+++ src/main/java/com/example/campusCircle/CampusCircleApplication.java
@@ -0,0 +1,13 @@
+package com.example.campusCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCircleApplication.class, args);
+	}
+
+}
